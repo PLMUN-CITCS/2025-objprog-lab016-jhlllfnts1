1ba8bef476b0e917bdd197576630b310af3944cb
Jhllfnts1 <fuentesjholl_bscs@plmun.edu.ph>
2025-03-03 13:23:46 +0800

    "Submitting OBJPROG Week 05 - Laboratory # 16"

---
+++ MethodOverloading.java
@@ -0,0 +1,24 @@
+public class MethodOverloading {
+
+    public static void main(String[] args) {
+        // Calling the overloaded methods
+        printValue(10);       // Calls the printValue(int number) method
+        printValue(3.14);     // Calls the printValue(double number) method
+        printValue("Hello!"); // Calls the printValue(String text) method
+    }
+
+    // Method to print an integer
+    public static void printValue(int number) {
+        System.out.println("Integer value: " + number);
+    }
+
+    // Method to print a double
+    public static void printValue(double number) {
+        System.out.println("Double value: " + number);
+    }
+
+    // Method to print a string
+    public static void printValue(String text) {
+        System.out.println("String value: " + text);
+    }
+}
